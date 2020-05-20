4aa0e17e90272a67a04e1c770072607f9c7e410e
Jairo Carmona <jairo.carmona@datapeers.co>
2020-05-19 20:46:48 -0500

    Primer ejercicio

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
+import { Observable } from 'rxjs';
 
 
-console.log('Hola Mundo!');
+// const obs$ = Observable.create();  // Posible forma de crear un observable, Poco usada;
+const obs$ = new Observable( subs => {
 
+    subs.next('Hola');
+    subs.next('Mundo');
+    subs.next('Hola');
+    subs.next('Navegador');
 
+    subs.complete();
 
+    //Estos valores no serán visualizados
+    subs.next('No se ve');
 
+});
 
-
+obs$.subscribe( console.log )
 
